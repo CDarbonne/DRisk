d994599788b72f935d068769d4a9e13a61a5d12d
Chris Darbonne <darbonnechris@gmail.com>
2016-05-05 21:00:39 -0500

    added test html file for card translations to be used as reference

--- Webpage/JavaScript Files/infoBoxes.js
+++ Webpage/JavaScript Files/infoBoxes.js
@@ -1,5 +1,6 @@
 //Get the modal
 var ContactModal = document.getElementById('ContactModal');
+ContactModal.style.display = "none";
 
 //Get the button to open the modal
 var contactBtn = document.getElementById("Contact");
@@ -12,7 +13,12 @@ var span = document.getElementsByClassName("closeModal")[0];
 
 //When the user clicks on the button, open the modal
 contactBtn.onclick = function() {
-	ContactModal.style.display = "block";
+	if (ContactModal.style.display == "none") {
+		ContactModal.style.display = "block";
+	}
+	else if (ContactModal.style.display == "block") {
+		ContactModal.style.display = "none";
+	}
 }
 
 //When the user clicks on <span> (X), close the modal
