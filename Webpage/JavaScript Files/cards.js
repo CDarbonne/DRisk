f4606b39dbb9ea64d41d787b32490f5cea3bc367
Lisa Tooke <Lisa Tooke>
2016-05-06 20:31:29 -0500

    push

--- Webpage/JavaScript Files/cards.js
+++ Webpage/JavaScript Files/cards.js
@@ -172,8 +172,6 @@ USA_CARDS[45] = new Card("WestVirginia", "USA", "South", 1, "images/Cards/USA/We
 USA_CARDS[46] = new Card("Wisconsin", "USA", "MidWest", 1, "images/Cards/USA/Wisconsin.png");
 USA_CARDS[47] = new Card("Wyoming", "USA", "West", 1, "images/Cards/USA/Wyoming.png");
 
-
-
 // create EASTEROS map cards (66 territories):
 EASTEROS_CARDS[0] = new Card("Ashford", "Easteros", "TheReach", 2, "images/Cards/Easteros/Ashford.png");
 EASTEROS_CARDS[1] = new Card("BearIsland", "Easteros", "TheNorth", 1, "images/Cards/Easteros/BearIsland.png");
@@ -243,19 +241,6 @@ EASTEROS_CARDS[64] = new Card("Winterfell", "Easteros", "TheNorth", 2, "images/C
 EASTEROS_CARDS[65] = new Card("Yronwood", "Easteros", "Dorne", 2, "images/Cards/Easteros/Yronwood.png");
 
 
-/*
-function createDeck(deck) {
-  var i;
-  var suits = ["c", "h", "s", "d"];
-
-  for(i = 0; i < 52; i++) {
-    deck[i] = new Card((i+1), (i%13)+1, suits[i%4]);
-  }
-
-  return deck;
-}
-*/
-
 function shuffleDeck(deck) {
   var shuffledDeck = [];
   var n = deck.length;
@@ -283,80 +268,3 @@ var card = {
 	filepath: "filepath"
 	
 };
-
-// card names for Easy map (12 Territories):
-var EASY_CARD_NAMES = [
-	
-	"Australia", "EasternEurope", "Lower SouthAmerica", "NorthAsia",
-	"NorthwestAfrica", "SoutheastAfrica", "SoutheastAsia", "SouthwestAsia",
-	"UpperNorthAmerica", "UpperSouthAmerica", "WesternEurope"
-	
-];
-
-// card names for Medium map (21 Territories):
-var MEDIUM_CARD_NAMES = [
-	
-	"Alaska", "Argentina", "AustralianIslands", "AustralianMainland",
-	"Brazil", "Canada", "Central America", "China", "EasternEurope",
-	"GreatBritain", "Greenland", "Iceland", "India", "Japan", "Madagascar",
-	"MiddleEast", "Northwest Africa", "Russia", "SoutheastAfrica",
-	"UnitedStates", "WesternEurope"
-	
-];
-
-// card names for Hard map (42 Territories):
-var HARD_CARD_NAMES = [
-
-	"Afghanistan", "Alaska", "Alberta", "Argentina", "Brazil", "CentralAmerica",
-	"China", "Congo", "EastAfrica", "EasternUnitedStates", "Egypt",
-	"Great Britian", "Greenland", "Iceland", "India", "Indonesia", "Irkutsk",
-	"Japan", "Kamchatka", "Madagascar", "MiddleEast", "Mongolia", "NewGuinea",
-	"North Africa", "NorthernEurope", "NorthwestTerritory", "Ontario", "Peru",
-	"Quebec", "Scandinavia", "Siam", "Siberia", "SouthAfrica", "SouthernEurope",
-	"Ukraine", "Ural", "Venezuela", "WesternAustralia", "WesternEurope",
-	"WesternUnitedStates", "Yakutsk"
-
-];
-
-// card names for Khorvaire map (16 Territories):
-var KHORVAIRE_CARD_NAMES = [
-
-	"Aundair", "Breland", "Darguun", "DemonWastes", "Drooam", "EldeenReaches",
-	"Karnath", "LhazaarPrincipalities", "Mournland", "MrorHolds", "OBarra",
-	"ShadowMarches", "Talenta Plains", "Thrane", "Valenar", "Zilargo"
-
-];
-
-// card names for USA map (48 Territories):
-var USA_CARD_NAMES = [
-
-	"Alabama", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
-	"Delaware", "Florida", "Georgia", "Idaho", "Illinois", "Indiana", "Iowa",
-	"Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
-	"Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", 
-	"Nevada", "NewHampshire", "NewJersey", "NewMexico", "NewYork",
-	"NorthCarolina", "NorthDakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-	"RhodeIsland", "SouthCarolina", "SouthDakota", "Tennessee", "Texas", "Utah",
-	"Vermont", "Virginia", "Washington", "WestVirginia", "Wisconsin", "Wyoming"
-
-];
-
-// card names for Easteros map (66 Territories):
-var EASTEROS_CARD_NAMES = [
-
-	"Ashford", "BearIsland", "Blackhaven", "BrightwaterKeep", "CapeKraken",
-	"CapeWrath", "CasterlyRock", "Cornfield", "CrackclawPoint", "Dragonstone",
-	"Duskendale", "GoldenGrove", "GoldenTooth", "GrassyVale", "Gulltown",
-	"Harrenhall", "HaystackHall", "Highgarden", "IronIslands", "KingsLanding",
-	"Kingswood", "Lannisport", "LonelyHills", "LongLake", "OldOak", "Oldtown",
-	"Prince's Pass", "Riverrun", "SaltShore", "Saltpans", "Sandstone",
-	"SeaDragonPoint", "SharpPoint", "SilverHill", "Skagos", "Skyreach",
-	"Starfall", "StoneySept", "StoneyShore", "StormsEnd", "Summerhall",
-	"Sunspear", "Tarth", "TheArbor", "TheBarrowlands", "TheBoneway",
-	"TheCrag", "TheEyrie", "TheFingers", "TheGift", "TheGreyCliffs",
-	"TheMountainsOfTheMoon", "TheNeck", "TheRills", "TheTrident",
-	"TheTwins", "TheWolfswood", "ThreeTowers", "TorrhensSquare", "Tumbleton",
-	"Uplands", "WestVale", "WhiteHarbor", "WidowsWatch", "Winterfell",
-	"Yronwood"
-
-];
